031b617cb3d584fdf02116198a03d29c2690301a
AlyssonSantos1 <salysson280@gmail.com>
2024-09-20 10:24:42 -0300

    updated

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -2,23 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Operations;
 use Illuminate\Http\Request;
 
 class ResultController extends Controller
 {
     public function create(Request $information) {
-        Request::create([
+        Operations::create([
             'name' => $information->name_client,
-            'accountbalance' =>$accountbalance->account_balance
+            'accountbalance' =>$information->account_balance
 
         ]);
         return "New User Create Sucess";
     }
 
     public function savings(Request $depositvalue, $id){
-        $value = $depositvalue++;
-        if ($depositvalue < 0  );
+        $value = $depositvalue++;{
+        if ($depositvalue > 0 )
         return "Deposit of $depositvalue value is sucessful";
+
+        else 
+        return "Invalid Value of Deposit";
+        }
     }
 
     
